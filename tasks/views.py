5295c956362f0d3090618c78c5df4a85c49a73f8
MugumoPerm <perminusk21@gmail.com>
2023-06-05 02:15:36 +0300

    add update

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 
+task = ["cook"]
+
+
 # Create your views here.
 def tasks(request):
-    return render(request, "tasks.html")
+    return render(request, "tasks.html", {
+        "task_list": task
+    })
 
 def add(request):
     return render(request, "add.html")
