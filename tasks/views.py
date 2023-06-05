d3c3c9be2485f339c704b110f11a55cb4eb46254
MugumoPerm <perminusk21@gmail.com>
2023-06-05 16:29:37 +0300

    update

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
-task = ["cook"]
+tasks = ["cook"]
+
+# class NewTaskForm(forms.Form):
+#     task = forms.CharField(label="New Task")
 
 
 # Create your views here.
 def tasks(request):
     return render(request, "tasks.html", {
-        "task_list": task
+        "task_list": tasks
     })
 
 def add(request):
